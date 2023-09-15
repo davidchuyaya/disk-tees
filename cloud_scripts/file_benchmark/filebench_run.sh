4c90f719b8b66947e79ebab629f3a26effc58636
David Chu <davidchuyaya@gmail.com>
2023-09-15 16:36:49 +0100

    Debugging filebench

--- cloud_scripts/file_benchmark/filebench_run.sh
+++ cloud_scripts/file_benchmark/filebench_run.sh
@@ -22,7 +22,8 @@ done
 PROJECT_DIR=/home/$USERNAME/disk-tees
 cd $PROJECT_DIR/cloud_scripts/file_benchmark
 
-CONFIGS=("seq-read-1th-16g.f" "seq-read-32th-16g.f" "seq-write-1th-16g.f" "seq-write-32th-16g.f" "seq-fsync-1th-16g.f" "seq-fsync-32th-16g.f" "seq-read-32th-64g.f" "rnd-read-32th-64g.f" "seq-write-32th-64g.f" "rnd-write-32th-64g.f" "seq-fsync-32th-64g.f" "rnd-fsync-32th-64g.f" "file-server.f" "mail-server.f" "web-server.f")
+# CONFIGS=("seq-read-1th-16g.f" "seq-read-32th-16g.f" "seq-write-1th-16g.f" "seq-write-32th-16g.f" "seq-fsync-1th-16g.f" "seq-fsync-32th-16g.f" "seq-read-32th-64g.f" "rnd-read-32th-64g.f" "seq-write-32th-64g.f" "rnd-write-32th-64g.f" "seq-fsync-32th-64g.f" "rnd-fsync-32th-64g.f" "file-server.f" "mail-server.f" "web-server.f")
+CONFIGS=("seq-read-1th-16g.f")
 DIR=$PROJECT_DIR/build/client0/shim
 
 RESULTS_DIR=$PROJECT_DIR/build/client0/results
@@ -30,7 +31,8 @@ mkdir -p $RESULTS_DIR
 for CONFIG in "${CONFIGS[@]}"; do
     echo "Running $CONFIG..."
     # Set mount directory
-    sed "'i $dir='$DIR" $CONFIG > $RESULTS_DIR/$CONFIG
+    sed "1i set "'$dir='"$DIR" $CONFIG > $RESULTS_DIR/$CONFIG
     # See output as it runs and pipe to log
-    filebench -f $CONFIG | tee $RESULTS_DIR/$CONFIG.log
+    sudo filebench -f $RESULTS_DIR/$CONFIG | tee $RESULTS_DIR/$CONFIG.log
+    sudo rm -rf $DIR/*
 done
