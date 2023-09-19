24f214f317564026abb790578c00f29570842953
David Chu <davidchuyaya@gmail.com>
2023-09-19 13:40:01 +0100

    Filebench too many files open fix

--- cloud_scripts/file_benchmark/filebench_install.sh
+++ cloud_scripts/file_benchmark/filebench_install.sh
@@ -24,6 +24,8 @@ wget https://github.com/filebench/filebench/releases/download/1.5-alpha3/fileben
 tar -xvf filebench-1.5-alpha3.tar.gz
 rm filebench-1.5-alpha3.tar.gz
 cd filebench-1.5-alpha3
+# Filebench needs to be modified to allow mail-server.f and web-server.f to load so many files: https://github.com/filebench/filebench/issues/90
+sed -i 's/FILEBENCH_NFILESETENTRIES\t(1024 \* 1024)/FILEBENCH_NFILESETENTRIES\t(1024 * 1024 * 10)/g' ipc.h
 # Install dependencies: https://github.com/filebench/filebench/wiki/Building-Filebench#building-filebench-from-the-git-repository
 sudo apt update
 sudo apt -y install bison flex build-essential
